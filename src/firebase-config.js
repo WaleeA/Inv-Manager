e9163101e8b5775d7c87120bc64bd6a041e2055b
Walee <walz.e3@gmail.com>
2023-04-22 19:40:06 +0100

    first commit

---
+++ src/firebase-config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCduSvzWIdgDaM3O6aalMG9BA03kfPS1_8",
+  authDomain: "inventory-walee.firebaseapp.com",
+  projectId: "inventory-walee",
+  storageBucket: "inventory-walee.appspot.com",
+  messagingSenderId: "977838943552",
+  appId: "1:977838943552:web:2cc517c96873a13ab6dea5",
+  measurementId: "G-N2PR41BCWN",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
